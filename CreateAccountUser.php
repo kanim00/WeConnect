e9d957d4693a091991829f91e2580ff9241df675
kanim00 <74391317+kanim00@users.noreply.github.com>
2022-04-24 15:10:41 +0000

    push

--- CreateAccountUser.php
+++ CreateAccountUser.php
@@ -34,10 +34,12 @@
 
             <!-- user input for registeration -->
     
-            <form id="register" class="input-group" method = "POST">
-                <input type="text" name = "user_id" id="user_id" class="input-field" placeholder="User Id" required>
+            <form id="register" class="input-group" method = "POST">    
+                <input type="text" name="first_name" id="first_name" class="input-field" placeholder="first name" required>
+                <input type="text" name="last_name" id="last_name" class="input-field" placeholder="last name" required>
+                <input type="text" name = "user_name" id="user_name" class="input-field" placeholder="User Name" required>
                 <input type="email" name ="email" id="email" class="input-field" placeholder="Email" required>
-                <input type="text" name="password" id="password" class="input-field" placeholder="Enter Password" required>
+                <input type="password" name="password" id="password" class="input-field" placeholder="Enter Password" required>
                 <!--<div><input type="checkbox" class="checkbox"><span> Remember Password</span></div>-->
                 <button type="submit" name="submit_btn" class="submit-btn">Register</button>
             </form>  
